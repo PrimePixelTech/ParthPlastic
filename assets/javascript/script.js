ee786964743d12174f73d035646e6770df6246de
Nirmal0189 <17nirmaldhorajiya@gmail.com>
2025-05-29 20:02:30 +0530

    BannerResponsiv

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -43,38 +43,61 @@ tl.from(".header-li", {
   opacity: 0,
 });
 
+
+
+
+
+
+
+
 //Banner Section
-const slides = document.querySelectorAll('.slide');
-const dots = document.querySelectorAll('.pagination .item');
-const prevArrow = document.querySelector('.arrow.prev');
-const nextArrow = document.querySelector('.arrow.next');
 
-let currentIndex = 0;
+$('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      responsive: {
+        0: {
+          items: 1
+        }
+      }
+    });
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.toggle('is-active', i === index);
-    dots[i].classList.toggle('is-active', i === index);
-  });
-  currentIndex = index;
-}
+// const slides = document.querySelectorAll('.slide');
+// const dots = document.querySelectorAll('.pagination .item');
+// const prevArrow = document.querySelector('.arrow.prev');
+// const nextArrow = document.querySelector('.arrow.next');
 
-dots.forEach((dot, index) => {
-  dot.addEventListener('click', () => showSlide(index));
-});
+// let currentIndex = 0;
 
-prevArrow.addEventListener('click', () => {
-  const newIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(newIndex);
-});
+// function showSlide(index) {
+//   slides.forEach((slide, i) => {
+//     slide.classList.toggle('is-active', i === index);
+//     dots[i].classList.toggle('is-active', i === index);
+//   });
+//   currentIndex = index;
+// }
 
-nextArrow.addEventListener('click', () => {
-  const newIndex = (currentIndex + 1) % slides.length;
-  showSlide(newIndex);
-});
+// dots.forEach((dot, index) => {
+//   dot.addEventListener('click', () => showSlide(index));
+// });
 
-// Optional: auto-slide every 5 seconds
-setInterval(() => {
-  const newIndex = (currentIndex + 1) % slides.length;
-  showSlide(newIndex);
-}, 4000);
+// prevArrow.addEventListener('click', () => {
+//   const newIndex = (currentIndex - 1 + slides.length) % slides.length;
+//   showSlide(newIndex);
+// });
+
+// nextArrow.addEventListener('click', () => {
+//   const newIndex = (currentIndex + 1) % slides.length;
+//   showSlide(newIndex);
+// });
+
+// // Optional: auto-slide every 5 seconds
+// setInterval(() => {
+//   const newIndex = (currentIndex + 1) % slides.length;
+//   showSlide(newIndex);
+// }, 4000);
